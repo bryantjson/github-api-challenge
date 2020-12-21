11371bbde923346d1280debc0ef3b164117c5bf9
Bryant Son <brson@redhat.com>
2020-12-21 11:23:24 -0600

    TEST

--- server-webhook.js
+++ server-webhook.js
@@ -4,9 +4,10 @@ const webhooks = new Webhooks({
   secret: "1234sec",
 });
 
-webhooks.on("*", ({ id, name, payload }) => {
+webhooks.onAny("*", ({ id, name, payload }) => {
   console.log(name, "event received");
 });
 
 require("http").createServer(webhooks.middleware).listen(3000);
 // can now receive webhook events at port 3000
+
