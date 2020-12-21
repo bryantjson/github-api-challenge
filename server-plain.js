1a8a073a77e11034c0c7bf0e8b41fa349c59056c
Bryant Son <brson@redhat.com>
2020-12-21 10:24:25 -0600

    CHANGE

---
+++ server-plain.js
@@ -0,0 +1,18 @@
+const secret = "1234sec";
+const repo = "/opt/app-root/src";
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+http.createServer(function (req, res) {
+  req.on('data', function(chunk) {
+      let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
+
+      if(req.headers['x-hub-signature'] == sig) {
+          exec('cd ' + repo + ' && git pull');
+      }
+  });
+
+  res.end();
+}).listen(3000);
