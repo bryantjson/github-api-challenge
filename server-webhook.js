34fe01c0dbd1e3e63adf9ab60ee5bc04c9ff1b87
Bryant Son <brson@redhat.com>
2020-12-21 16:23:18 -0600

    CHANGE

--- server-webhook.js
+++ server-webhook.js
@@ -3,9 +3,7 @@ var createHandler = require('github-webhook-handler')
 var handler = createHandler({ path: '/webhook', secret: '1234sec' })
 const { Octokit } = require("@octokit/core");
 
-const ocktokit = new Octokit({ auth: `1234sec`});
-
-
+const octokit = new Octokit({ auth: `1234sec`});
 
 const start = async function(a,b) {
   const response = await octokit.request("GET /orgs/{org}/repos", {
