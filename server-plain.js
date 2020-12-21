5cca606b25f07d022ca98a39a0cfe1d69a0dce9d
Bryant Son <brson@redhat.com>
2020-12-21 10:31:19 -0600

    TEST

--- server-plain.js
+++ server-plain.js
@@ -1,7 +1,7 @@
 const secret = "1234sec";
 const repo = "/opt/app-root/src";
 
-// TEST
+
 const http = require('http');
 const crypto = require('crypto');
 const exec = require('child_process').exec;
@@ -10,6 +10,7 @@ http.createServer(function (req, res) {
   req.on('data', function(chunk) {
       let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
+      console.log("NEW TEST");
       if(req.headers['x-hub-signature'] == sig) {
           exec('cd ' + repo + ' && git pull');
       }
