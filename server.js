00f3f83640080c178de3024a2025426a7dc83f67
Bryant Son <brson@redhat.com>
2020-12-20 23:12:40 -0600

    ADDING fix

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.post('/payload', (request, response) => {
   console.log("REQUEST FROM GITHUB: " + JSON.stringify(request.body.read));
-  response.send('Hello from Express!');
+  response.send('Hello from GitHub!');
 });
 
 app.listen(port, (err) => {
